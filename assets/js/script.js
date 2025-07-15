31aa5e8934602e943f785b8c1b5c428ce4815f63
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2025-07-15 17:16:33 +0500

    card section (courses) and special consultation section fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// NAVBAR BURGER MENU START
 document.addEventListener("DOMContentLoaded", function () {
   const burgerBtn = document.getElementById("burger-btn");
   const navMenu = document.getElementById("nav-menu");
@@ -7,22 +8,38 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// video scripts
-$(document).ready(function() {
-// get video source from data-src button
-var $videoSrc;  
-$('.video-btn').click(function() {
-    $videoSrc = $(this).data( "src" );
-});
-console.log($videoSrc);
-// autoplay video on modal load  
-$('#myModal').on('shown.bs.modal', function (e) {
-// youtube iframe configuration and settings
-$("#video").attr('src',$videoSrc + "?autoplay=1&rel=0&controls=1&loop=1&modestbranding=1"); 
-})
-// stop playing the youtube video when modal closed
-$('#myModal').on('hide.bs.modal', function (e) {
+// NAVBAR BURGER MENU END
+
+//============================================================================================
+
+// VIDEO SCRIPT START
+$(document).ready(function () {
+  // get video source from data-src button
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+  // autoplay video on modal load
+  $("#myModal").on("shown.bs.modal", function (e) {
+    // youtube iframe configuration and settings
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&rel=0&controls=1&loop=1&modestbranding=1"
+    );
+  });
+  // stop playing the youtube video when modal closed
+  $("#myModal").on("hide.bs.modal", function (e) {
     // stop video
-    $("#video").attr('src',$videoSrc); 
-}) 
+    $("#video").attr("src", $videoSrc);
+  });
 });
+// VIDEO SCRIPT END
+
+//============================================================================================
+
+// GSAP START
+
+// GSAP END
+
+//============================================================================================
